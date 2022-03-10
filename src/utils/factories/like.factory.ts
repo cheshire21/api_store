feb398997cfc76dc075c264360a57e9f069a04c1
cheshire21 <coren114@gmail.com>
2022-03-10 00:22:15 -0500

    test: added likes factory

---
+++ src/utils/factories/like.factory.ts
@@ -0,0 +1,27 @@
+import { Like, Prisma } from '@prisma/client';
+import { datatype } from 'faker';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Abstractfactory } from './abstract.factory';
+
+type LikeInput = Prisma.LikeCreateInput;
+
+export class LikeFactory extends Abstractfactory<Like> {
+  constructor(protected prisma: PrismaService) {
+    super();
+  }
+
+  async make(data: LikeInput): Promise<Like> {
+    const like = await this.prisma.like.create({
+      data: {
+        like: data.like ?? datatype.boolean(),
+        user: data.user,
+        product: data.product,
+      },
+    });
+    return like;
+  }
+
+  makeMany(quanty: number, data: LikeInput): Promise<Like[]> {
+    return Promise.all([...Array(quanty)].map(() => this.make(data)));
+  }
+}
