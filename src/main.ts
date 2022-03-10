b8c36d3c494f703ced56e462fce8d6f5efd59b3a
cheshire21 <coren114@gmail.com>
2022-03-09 22:56:25 -0500

    feat: added nest swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
@@ -11,8 +12,24 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   const prismaService: PrismaService = app.get(PrismaService);
+
   prismaService.enableShutdownHook(app);
+
+  const config = new DocumentBuilder()
+    .setTitle('Api Store')
+    .setDescription('Api Store for products, carts and orders')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addTag('products')
+    .addTag('carts')
+    .addTag('orders')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
