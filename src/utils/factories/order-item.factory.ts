a9f32f95a620f80ccc77a155603a210ff02fe857
cheshire21 <coren114@gmail.com>
2022-03-12 23:48:11 -0500

    feat: added order item factory

---
+++ src/utils/factories/order-item.factory.ts
@@ -0,0 +1,31 @@
+import { OrderItem, Prisma } from '@prisma/client';
+import { PrismaService } from '../../prisma/prisma.service';
+import { Abstractfactory } from './abstract.factory';
+
+type OrderItemInput = Prisma.OrderItemCreateInput;
+
+export class OrderItemFactory extends Abstractfactory<OrderItem> {
+  constructor(protected prisma: PrismaService) {
+    super();
+  }
+  async make(data: OrderItemInput, product?: any): Promise<OrderItem> {
+    const cartItem = await this.prisma.orderItem.create({
+      data: {
+        order: data.order,
+        product: product ?? data.product,
+        quantity: data.quantity,
+        unitPrice: data.unitPrice,
+        totalPrice: data.totalPrice,
+      },
+    });
+    return cartItem;
+  }
+
+  async makeMany(
+    quanty: number,
+    data: OrderItemInput,
+    product?: any,
+  ): Promise<OrderItem[]> {
+    return Promise.all([...product].map((prod) => this.make(data, prod)));
+  }
+}
