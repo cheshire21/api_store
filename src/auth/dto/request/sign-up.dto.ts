8d08199eaf7b390aba01b6941b219c2b4e2e1105
cheshire21 <coren114@gmail.com>
2022-03-19 20:23:26 -0500

    fix: added sign-up.dto

---
+++ src/auth/dto/request/sign-up.dto.ts
@@ -0,0 +1,63 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude, Expose } from 'class-transformer';
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { address, internet, name } from 'faker';
+import { Role } from '../../../utils/enums';
+
+@Exclude()
+export class SignUpDto {
+  @ApiProperty({
+    example: name.firstName(),
+  })
+  @Expose()
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @ApiProperty({
+    example: name.lastName(),
+  })
+  @Expose()
+  @IsString()
+  lastName: string;
+
+  @ApiProperty({
+    example: address.direction(),
+  })
+  @Expose()
+  @IsNotEmpty()
+  @IsString()
+  address: string;
+
+  @ApiProperty({
+    example: internet.userName(),
+  })
+  @Expose()
+  @IsNotEmpty()
+  @IsString()
+  userName: string;
+
+  @ApiProperty({
+    example: internet.email(),
+  })
+  @Expose()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    example: internet.password(),
+  })
+  @Expose()
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @ApiProperty({
+    example: 'M',
+  })
+  @Expose()
+  @IsNotEmpty()
+  @IsEnum(Role)
+  role: Role;
+}
