774b898448ac56fb1a82f0225587e73dde17597e
cheshire <coren114@gmail.com>
2022-03-23 17:22:39 -0500

    feat: added guards for graphQL

---
+++ src/auth/guards/gql-role.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { Role } from '../../common/enums';
+import { ROLES_KEYS } from '../decorators/role.decorator';
+
+@Injectable()
+export class GqlRolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEYS, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+    const ctx = GqlExecutionContext.create(context);
+    const { req } = ctx.getContext();
+    console.log(req.user);
+    return requiredRoles.some((role) => req.user.role?.includes(role));
+  }
+}
