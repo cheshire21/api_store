7fc1d47c12f52f62de1b361926df1a4a256d32b4
cheshire21 <coren114@gmail.com>
2022-03-24 16:41:27 -0500

    feat: added image input file

--- src/products/dto/input/image.input.ts
+++ src/products/dto/input/image.input.ts
@@ -1,9 +1,10 @@
-import { InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 import { IsEnum } from 'class-validator';
 import { ImageType } from 'src/common/enums';
 
 @InputType()
 export class ImageInput {
+  @Field()
   @IsEnum(ImageType)
   type: ImageType;
 }
