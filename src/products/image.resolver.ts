d9dbae4fba2f031e530abe21f5eead0fdcdf6c51
cheshire21 <coren114@gmail.com>
2022-03-24 16:49:18 -0500

    feat: added field resolver to product in image object type

---
+++ src/products/image.resolver.ts
@@ -0,0 +1,13 @@
+import { Resolver, ResolveField, Parent } from '@nestjs/graphql';
+import { Image } from './models/image.model';
+import { ProductsService } from './products.service';
+
+@Resolver(() => Image)
+export class ImagesResolver {
+  constructor(private productsService: ProductsService) {}
+  @ResolveField()
+  async product(@Parent() image) {
+    const { productId } = image;
+    return await this.productsService.getOne(productId);
+  }
+}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -6,6 +6,7 @@ import { RolesGuard } from 'src/auth/guards/role.guard';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { FilesService } from '../files/file.service';
 import { LikesService } from '../likes/likes.service';
+import { ImagesResolver } from './image.resolver';
 import { ProductsController } from './products.controller';
 import { ProductsResolver } from './products.resolver';
 import { ProductsService } from './products.service';
@@ -21,6 +22,7 @@ import { ProductsService } from './products.service';
     JwtAuthGuard,
     RolesGuard,
     ProductsResolver,
+    ImagesResolver,
   ],
 })
 export class ProductsModule {}
