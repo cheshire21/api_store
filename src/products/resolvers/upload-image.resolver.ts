cefb8e53e8d5a4ec9a02e36325cf43e2a5c7d949
cheshire21 <coren114@gmail.com>
2022-03-28 15:26:30 -0500

    feat: added upload image model

---
+++ src/products/resolvers/upload-image.resolver.ts
@@ -0,0 +1,14 @@
+import { Resolver, ResolveField, Parent } from '@nestjs/graphql';
+import { UploadImage } from '../models/upload-image.model';
+import { ProductsService } from '../products.service';
+
+@Resolver(() => UploadImage)
+export class UploadImagesResolver {
+  constructor(private productsService: ProductsService) {}
+  @ResolveField()
+  async product(@Parent() image) {
+    const { productId } = image;
+
+    return await this.productsService.getOne(productId);
+  }
+}
