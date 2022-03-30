572e6901a333f690cd592a6c5f89f5c6ab32262f
cheshire21 <coren114@gmail.com>
2022-03-30 16:49:37 -0500

    feat: added send email module in orders module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -9,17 +9,29 @@ import { OrdersResolver } from './resolvers/orders.resolver';
 import { OrdersService } from './orders.service';
 import { OrderItemResolver } from './resolvers/order-item.resolver';
 import { UsersModule } from 'src/users/users.module';
+import { OrderCreatedListener } from './listeners/order-created.listener';
+import { SendEmailsModule } from 'src/send-emails/send-emails.module';
+import { FilesService } from 'src/files/file.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, ProductsModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ProductsModule,
+    SendEmailsModule,
+    ConfigModule,
+  ],
   controllers: [OrdersController],
   providers: [
     OrdersService,
     PrismaService,
+    FilesService,
     JwtAuthGuard,
     RolesGuard,
     OrdersResolver,
     OrderItemResolver,
+    OrderCreatedListener,
   ],
 })
 export class OrdersModule {}
